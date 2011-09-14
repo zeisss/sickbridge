07f9a86f14d74fc9095b301a73ebc931da7aae4b
Alexej Disterhoft <alexej.disterhoft@gmail.com>
2011-09-15 03:17:59 +0800

    jdownloader.py: switch from webinterface to remote.
    You have to activate the remote extension in JDownloader and change your url/port to get it working again.

--- sickbridge/jdownloader.py
+++ sickbridge/jdownloader.py
@@ -1,20 +1,20 @@
 import urllib
 
 def add_link(jdownloader_url, link):
-	url = "%s/link_adder.tmpl" % jdownloader_url
-	
-	data = urllib.urlencode({'do':'Add', 'addlinks': link})
-	
-	s = urllib.urlopen(url, data)
-	
-	code = s.getcode()
-	#print "Jdownloader: %s" % code
-	
-	return code == 200
+    if jdownloader_url[-1] != '/':
+        jdownloader_url = '%s/' % jdownloader_url
+    try:
+        request = urllib.urlopen('%s/action/add/links/%s' % (jdownloader_url, link))
+    except IOError:
+        return False
+    return request.getcode() == 200
 
-def in_queue(url, filename):
-	s = urllib.urlopen(url)
-	html = s.read()
-	s.close()
-	
-	return html.find(">%s<" % filename) >= 0
+def in_queue(jdownloader_url, filename):
+    if jdownloader_url[-1] != '/':
+        jdownloader_url = '%s/' % jdownloader_url
+    try:
+        request = urllib.urlopen('%sget/downloads/alllist' % jdownloader_url)
+    except IOError:
+        return False
+    return request.read().find('%s' % filename) >= 0
+
