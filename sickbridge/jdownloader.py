12327fc9e75fe2d67be498b183fe91fb75a17ea0
Alexej Disterhoft <alexej.disterhoft@gmail.com>
2011-09-15 18:18:40 +0800

    add check if JDownloader is running.

--- sickbridge/jdownloader.py
+++ sickbridge/jdownloader.py
@@ -1,6 +1,24 @@
 import urllib
 
+def is_jd_on(jdownloader_url):
+    '''
+    Check if JDownloader is running (the app, not the download)
+    '''
+
+    if jdownloader_url[-1] != '/':
+        jdownloader_url = '%s/' % jdownloader_url
+    try:
+        request = urllib.urlopen('%sget/rcversion' % jdownloader_url)
+    except IOError:
+        return False
+    return request.getcode() == 200
+
 def add_link(jdownloader_url, link):
+    '''
+    Add given link to JDownloader.
+    Returns False if something went wrong
+    '''
+
     if jdownloader_url[-1] != '/':
         jdownloader_url = '%s/' % jdownloader_url
     try:
@@ -10,6 +28,11 @@ def add_link(jdownloader_url, link):
     return request.getcode() == 200
 
 def in_queue(jdownloader_url, filename):
+    '''
+    Check if the given file is already in the JDownloader queue.
+    Returns False if not in queue or something went wrong.
+    '''
+
     if jdownloader_url[-1] != '/':
         jdownloader_url = '%s/' % jdownloader_url
     try:
